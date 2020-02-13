1ae5c8f9b616113d4737f02f270e0e9d52d15cc0
HuanZ <huanzhangrain@gmail.com>
2020-02-13 14:17:35 +0800

    Player and Exit

--- app/src/main/java/com/example/mazegame/GameView.java
+++ app/src/main/java/com/example/mazegame/GameView.java
@@ -5,6 +5,7 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
 import androidx.annotation.Nullable;
@@ -14,11 +15,17 @@ import java.util.Random;
 import java.util.Stack;
 
 public class GameView extends View {
+
+    private enum Direction{
+        UP, DOWN, LEFT, RIGHT
+    }
+
     private Cell[][] cells;
+    private Cell player, exit;
     private static final int COLS = 7, ROWS = 10;
     private static final float WALL_THICKNESS = 4;
     private float cellSize, hMargin, vMargin;
-    private Paint wallPaint;
+    private Paint wallPaint, playerPaint, exitPaint;
     private Random random;
 
     public GameView(Context context, @Nullable AttributeSet attrs) {
@@ -27,6 +34,12 @@ public class GameView extends View {
         wallPaint = new Paint();
         wallPaint.setColor(Color.BLACK);
         wallPaint.setStrokeWidth(WALL_THICKNESS);
+
+        playerPaint = new Paint();
+        playerPaint.setColor(Color.RED);
+
+        exitPaint = new Paint();
+        exitPaint.setColor(Color.BLUE);
 
         random = new Random();
 
@@ -104,6 +117,9 @@ public class GameView extends View {
             }
         }
 
+        player = cells[0][0];
+        exit = cells[COLS-1][ROWS-1];
+
         // Default cells
         current = cells[0][0];
         current.visited = true;
@@ -173,6 +189,109 @@ public class GameView extends View {
                             wallPaint);
             }
         }
+
+        float margin = cellSize/10;
+
+        // Draw player
+        canvas.drawRect(
+                player.col*cellSize+margin,
+                player.row*cellSize+margin,
+                (player.col+1)*cellSize-margin,
+                (player.row+1)*cellSize-margin,
+                playerPaint);
+
+        // Draw exit
+        canvas.drawRect(
+                exit.col*cellSize+margin,
+                exit.row*cellSize+margin,
+                (exit.col+1)*cellSize-margin,
+                (exit.row+1)*cellSize-margin,
+                exitPaint);
+    }
+
+    private void movePlayer(Direction direction){
+        switch (direction){
+            case UP:
+                // player can only move up if there is no wall
+                if(!player.topWall){
+                    player = cells[player.col][player.row-1];
+                }
+                break;
+            case DOWN:
+                if(!player.bottomWall){
+                    player = cells[player.col][player.row+1];
+                }
+                break;
+            case LEFT:
+                if(!player.leftWall){
+                    player = cells[player.col-1][player.row];
+                }
+                break;
+            case RIGHT:
+                if(!player.rightWall){
+                    player = cells[player.col+1][player.row];
+                }
+                break;
+        }
+
+        checkExit();
+        // Call draw method as soon as possible
+        invalidate();
+    }
+
+    private void checkExit(){
+        if(player == exit){
+            createMaze();
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if(event.getAction() == MotionEvent.ACTION_DOWN){
+            return true;
+        }
+
+        if(event.getAction() == MotionEvent.ACTION_MOVE){
+            float x = event.getX();
+            float y = event.getY();
+
+            float playerCenterX = hMargin + (player.col+0.5f)*cellSize;
+            float playerCenterY = vMargin + (player.row+0.5f)*cellSize;
+
+            float dx = x - playerCenterX;
+            float dy = y - playerCenterY;
+
+            float absDx = Math.abs(dx);
+            float absDy = Math.abs(dy);
+
+            if(absDx > cellSize || absDy > cellSize){
+
+                if(absDx > absDy){
+                    // move in x-direction
+                    if(dx > 0){
+                        // move to the right
+                        movePlayer(Direction.RIGHT);
+                    }
+                    else{
+                        // move to the left
+                        movePlayer(Direction.LEFT);
+                    }
+                }
+                else{
+                    // move in y-direction
+                    if(dy > 0){
+                        // move down
+                        movePlayer(Direction.DOWN);
+                    }
+                    else{
+                        // move up
+                        movePlayer(Direction.UP);
+                    }
+                }
+            }
+            return true;
+        }
+        return super.onTouchEvent(event);
     }
 
     private class Cell{
