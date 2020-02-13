1fabb872921f1a88c388c711cf67d1306eb28230
HuanZ <huanzhangrain@gmail.com>
2020-02-13 13:22:17 +0800

    Draw line of the maze

--- app/src/main/java/com/example/mazegame/GameView.java
+++ app/src/main/java/com/example/mazegame/GameView.java
@@ -3,6 +3,7 @@ package com.example.mazegame;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -11,9 +12,18 @@ import androidx.annotation.Nullable;
 public class GameView extends View {
     private Cell[][] cells;
     private static final int COLS = 7, ROWS = 10;
+    private static final float WALL_THICKNESS = 4;
+    private float cellSize, hMargin, vMargin;
+    private Paint wallPaint;
 
     public GameView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+
+        wallPaint = new Paint();
+        wallPaint.setColor(Color.BLACK);
+        wallPaint.setStrokeWidth(WALL_THICKNESS);
+
+        createMaze();
     }
 
     private void createMaze(){
@@ -29,6 +39,55 @@ public class GameView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         canvas.drawColor(Color.GREEN);
+
+        // Get size of canvas
+        int width = getWidth();
+        int height = getHeight();
+
+        // Calculate the cell size
+        if(width/height < COLS/ROWS)
+            cellSize = width/(COLS+1);
+        else
+            cellSize = height/(ROWS+1);
+
+        // Calcualte the margin
+        hMargin = (width - COLS*cellSize) / 2;
+        vMargin = (height - ROWS*cellSize) / 2;
+
+        canvas.translate(hMargin, vMargin);
+
+        for(int x=0; x < COLS; x++){
+            for(int y=0; y < ROWS; y++){
+                if(cells[x][y].topWall)
+                    canvas.drawLine(
+                            x*cellSize,
+                            y*cellSize,
+                            (x+1)*cellSize,
+                            y*cellSize,
+                            wallPaint);
+                if(cells[x][y].leftWall)
+                    canvas.drawLine(
+                            x*cellSize,
+                            y*cellSize,
+                            x*cellSize,
+                            (y+1)*cellSize,
+                            wallPaint);
+                if(cells[x][y].bottomWall)
+                    canvas.drawLine(
+                            x*cellSize,
+                            (y+1)*cellSize,
+                            (x+1)*cellSize,
+                            (y+1)*cellSize,
+                            wallPaint);
+                if(cells[x][y].rightWall)
+                    canvas.drawLine(
+                            (x+1)*cellSize,
+                            y*cellSize,
+                            (x+1)*cellSize,
+                            (y+1)*cellSize,
+                            wallPaint);
+            }
+        }
     }
 
     private class Cell{
